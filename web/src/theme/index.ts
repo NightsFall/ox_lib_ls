120f95e7a8b1f2a4f69dd7a49042296ab7a257de
NightsFall <125892967+NightsFall@users.noreply.github.com>
2025-03-01 19:52:03 +0100

    Update index.ts
    Signed-off-by: NightsFall <125892967+NightsFall@users.noreply.github.com>

--- web/src/theme/index.ts
+++ web/src/theme/index.ts
@@ -2,9 +2,81 @@ import { MantineThemeOverride } from '@mantine/core';
 
 export const theme: MantineThemeOverride = {
   colorScheme: 'dark',
-  fontFamily: 'Roboto',
-  shadows: { sm: '1px 1px 3px rgba(0, 0, 0, 0.5)' },
+  fontFamily: 'Montserrat',
+  shadows: { sm: '0px 0px 0px rgba(0, 0, 0, 0.0)' },
   components: {
+    Modal: {
+      styles: {
+        modal: {
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        },
+      },
+    },
+
+    TextInput: {
+      styles: {
+        input: {
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        },
+      },
+    },
+    
+    TimeInput: {
+      styles: {
+        input: {
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        },
+      },
+    },
+
+    Select: {
+      styles: {
+        input: {
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        },
+      },
+    },
+
+
+    MultiSelect: {
+      styles: {
+        input: {
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        },
+      },
+    },
+
+    NumberInput: {
+      styles: {
+        input: {
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        },
+
+        
+      },
+    },
+
+    Checkbox: {
+      styles: {
+        input: {
+          color:'white',
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        },
+      },
+    },
+
+    PasswordInput: {
+      styles: {
+        input: {
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        },
+        icon: {
+          backgroundColor: '#0066ff',
+          color:'white',
+        },
+      },
+    },
+
     Button: {
       styles: {
         root: {
